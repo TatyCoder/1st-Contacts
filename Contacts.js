7cc5d2092dd9bf55a95f2db9bce388e355949858
TatyCoder <tmilanes@gmail.com>
2020-03-16 16:34:02 -0400

    first commit

---
+++ Contacts.js
@@ -0,0 +1,28 @@
+var contacts = [{
+        name: "Name 1",
+        address: {
+            street: "street1",
+            city: "city1",
+            zip: "zip1"
+        },
+        phone: "phone1"
+    },
+    {
+        name: "Name 2",
+        address: {
+            street: "street2",
+            city: "city2",
+            zip: "zip2",
+        },
+        phone: "phone2"
+    },
+    {
+        name: "Name 3",
+        address: {
+            street: "street3",
+            city: "city3",
+            zip: "zip3",
+        },
+        phone: "phone3"
+    }
+]
